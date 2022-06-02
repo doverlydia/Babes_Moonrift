c7851efdf1b3267ade1906165813cbfed09f369a
RandyL1 <randylawrenson@gmail.com>
2022-06-02 16:37:48 -0400

    Player Move
    I got basic player movement to work.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+        
+        rb.velocity = new Vector2(xMove * speed, yMove * speed);
+    }
+}
